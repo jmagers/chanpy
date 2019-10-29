869e36a5660f09ceb20ef6ef8f0b02a86a391900
Jake Magers <jmagers12@gmail.com>
2019-10-29 00:23:32 -0700

    Add close() & non-blocking get()/put() to channel

--- channel.py
+++ channel.py
@@ -1,7 +1,5 @@
-import queue
 import threading
 from collections import deque
-from toolz import complement
 
 
 class Empty(Exception):
@@ -42,38 +40,70 @@ class BufferedChannel:
         self._notEmpty = threading.Condition(self._lock)
         self._notFull = threading.Condition(self._lock)
         self._buffer = buffer
+        self._isClosed = False
 
-    def get(self):
+    def get(self, block=True):
         with self._notEmpty:
-            self._notEmpty.wait_for(complement(self._buffer.isEmpty))
-            item = self._buffer.get()
-            self._notFull.notify()
-            return item
+            if not block and self._buffer.isEmpty():
+                return None
+            self._notEmpty.wait_for((lambda: self._isClosed or
+                                     not self._buffer.isEmpty()))
+            if not self._buffer.isEmpty():
+                item = self._buffer.get()
+                self._notFull.notify()
+                return item
+            return None
 
-    def put(self, item):
+    def put(self, item, block=True):
+        if item is None:
+            raise TypeError('item cannot be None')
         with self._notFull:
-            self._notFull.wait_for(complement(self._buffer.isFull))
+            if not block and self._buffer.isFull():
+                return False
+            self._notFull.wait_for((lambda: self._isClosed or
+                                    not self._buffer.isFull()))
+            if self._isClosed:
+                return False
             self._buffer.put(item)
             self._notEmpty.notify()
+            return True
+
+    def close(self):
+        with self._lock:
+            self._isClosed = True
+            self._notEmpty.notify_all()
+            self._notFull.notify_all()
 
 
 class UnbufferedChannel:
     def __init__(self):
-        self._itemQ = queue.Queue(1)
-        self._completeQ = queue.Queue(1)
+        self._itemCh = BufferedChannel(FixedBuffer(1))
+        self._completeCh = BufferedChannel(FixedBuffer(1))
         self._consumerLock = threading.Lock()
         self._producerLock = threading.Lock()
+        self._isConsumerWaiting = False
 
-    def get(self):
+    def get(self, block=True):
         with self._consumerLock:
-            item = self._itemQ.get()
-            self._completeQ.put(None)
+            self._isConsumerWaiting = block
+            item = self._itemCh.get(block=block)
+            if item is None or not self._completeCh.put('consumer finished'):
+                return None
             return item
 
-    def put(self, item):
+    def put(self, item, block=True):
+        if item is None:
+            raise TypeError('item cannot be None')
         with self._producerLock:
-            self._itemQ.put(item)
-            self._completeQ.get()
+            if not block and not self._isConsumerWaiting:
+                return False
+            self._itemCh.put(item)
+            self._isConsumerWaiting = False
+            return self._completeCh.get() is not None
+
+    def close(self):
+        self._itemCh.close()
+        self._completeCh.close()
 
 
 def chan(buf=0):
